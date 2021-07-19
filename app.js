e1f685a7dbc9e6769a1e51262bc8b69c2b3f5e56
Rachel Johnson <rachelktyjohnson@gmail.com>
2021-07-19 14:36:22 +1000

    set up middleware

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -43,18 +43,24 @@ let sequelize = require('./models').sequelize;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  //next(createError(404));
+  const error = new Error();
+  error.status = 404;
+  error.message = "Oops! Page not found.";
+  res.render('page_not_found', {error})
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  err.status = err.status || 500;
+  err.message = err.message || "Sorry! There was an unexpected error on the server.";
+  console.log(err.status);
+  console.log(err.message);
+  res.render('error', {error:err});
 });
 
 module.exports = app;
